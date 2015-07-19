30af1060646be97b06b074e18dea145d706b30cd
bowmanjeffs <bowmanjs@gmail.com>
2015-07-19 14:48:51 -0400

    include test.fasta
    Setup required a small fasta file of reads in order to generate a
    phyloxml format tree with pplacer and guppy.  Added test.fasta, a small
    fasta file of reads from a recent analysis.  The paprica.sh script is
    setup to use this.

--- paprica.sh
+++ paprica.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-#### these are the critical steps for using genome finder ####
+#### these are the critical steps for using paprica ####
 #### be sure to check the beginning of each script for user editable variables ####
-#### you will need to update the version numbers in this script ####
 
-query=file-without-extension
+## currently set to run with provided test.fasta, after initial
+## setup replace test with the name of your query file, excluding
+## the extension
+query=test
+
+## you may need to update the version number in this script
+version=0.1
 
 ## 1. download genomes, combine elements, extract 16S
-python paprica_make_ref.py
+python paprica_make_ref_v${version}.py
 
 ## 2. make a reference package from 16S
-python paprica_place_it.py combined_16S
+python paprica_place_it_v${version}.py combined_16S
 
 ## 3. run first analysis 
-python paprica_place_it.py $query combined_16S
+python paprica_place_it_v${version}.py $query combined_16S
 
 ## 4. get csv of placements
 guppy_to_csv --point-mass --pp -o [query].csv $query.combined_16S.pplacer.filter.jplace
@@ -22,10 +27,10 @@ guppy_to_csv --point-mass --pp -o [query].csv $query.combined_16S.pplacer.filter
 guppy fat --point-mass --pp --node-numbers -o $query.fat $query.combined_16S.pplacer.filter.jplace
 
 ## 6. build the reference database - you need the node numbers from the previous step for this
-python paprica_build_core_genomes.py $query.fat
+python paprica_build_core_genomes_v${version}.py $query.fat
 
 ## 7. find genomes represented in query
-python paprica_get_genomes.py $query.csv $query
+python paprica_get_genomes_v${version}.py $query.csv $query
 
 ## 8. build PGDB
 chmod a+x generate_pgdbs.sh
