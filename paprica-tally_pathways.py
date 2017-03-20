f14b1fa8e4b497ff85a6926275b274e98b67df78
bowmanjeffs <bowmanjs@gmail.com>
2017-03-20 15:49:14 -0700

    grrrrrrr

--- paprica-tally_pathways.py
+++ paprica-tally_pathways.py
@@ -50,7 +50,6 @@ import sys
 import os
 
 paprica_path = os.path.dirname(os.path.abspath(__file__)) + '/' # The location of the actual paprica scripts.
-#paprica_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)) + '/' 
 cwd = os.getcwd() + '/'  # The current working directory
     
 ## Parse command line arguments.
